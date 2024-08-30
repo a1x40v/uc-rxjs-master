0c0e2a4d15e41ae400b9b6c21865b4e225eb7ed1
a1x40v <a1x40v@gmail.com>
2024-08-30 16:56:08 +0300

    Lab1. Manage application loading with Subject

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,31 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 
-export default {
-  input: 'src/main.ts',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'iife',
-    name: 'MyBundle',
+export default [
+  {
+    input: 'src/main.ts',
+    output: {
+      file: 'dist/bundle.js',
+      format: 'iife',
+      name: 'MyBundle',
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ]
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-  ]
-};
+  {
+    input: 'src/lab1/lab1.ts',
+    output: {
+      file: 'dist/lab1.bundle.js',
+      format: 'iife',
+      name: 'Lab1Bundle',
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ]
+  },
+];
